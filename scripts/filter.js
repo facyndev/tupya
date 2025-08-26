c4f753aed01a78b147c9b1343dd75eb0959bd832
FacynDev <facundogrieco87@gmail.com>
2025-08-26 20:41:33 -0300

    cambio: Filtros para la pagina pedidos, falta terminar

---
+++ scripts/filter.js
@@ -0,0 +1,53 @@
+// Filtrado de los pedidos
+const pageUrl = new URL(window.location)
+const params = new URLSearchParams(window.location.search);
+const filterParams = params.get('filter');
+
+// Obtenemos todos los botones que coincidan con la ID.
+const filterButtonsElements = document.querySelectorAll('#filterButton');
+
+filterButtonsElements.forEach((el) => {
+    // A cada elemento la añadimos un evento
+    el.addEventListener('click', (e) => {
+        const filterType = e.currentTarget.attributes["data-filter"]; 
+        const actualValues = params.getAll('filter');
+
+        if(actualValues.includes(filterType.value)) {
+            params.delete('filter', filterType.value)
+        } else {
+            params.append('filter', filterType.value)
+        }
+
+        updateFilters()
+    })
+})
+
+
+
+function updateFilters() {
+    const actualValues = params.getAll('filter');
+  
+    // Indicamos los nuevos parametros de busqueda a nuestra URL
+    pageUrl.search = params.toString()
+
+    // Creamos la url que posteriormente sera que la reemplza
+    const newUrl = `${pageUrl.pathname}?${params.toString()}${pageUrl.hash}`;
+
+    // Reemplazamos la URL actua con los nuevos parametros de busqueda
+    history.replaceState(null, '', newUrl)
+
+    filterButtonsElements.forEach((el) => {
+        if(actualValues.includes(el.attributes["data-filter"].value)) {
+            el.classList.remove('bg-gray-200', 'text-black')
+            el.classList.add('bg-[var(--primary-color)]', 'text-[var(--text-color)]')
+        } else {
+            el.classList.add('bg-gray-200', 'text-black')
+            el.classList.remove('bg-[var(--primary-color)]', 'text-[var(--text-color)]')
+        }
+    })
+}
+
+// Cuando termine de cargar todo el contenido de la pagina, actualiza los filtros
+window.addEventListener('DOMContentLoaded', () => {
+    updateFilters()
+})
