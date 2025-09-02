2aef6d2bdc5b6a5b718a8761bfcf9e3acfb51e31
Ezequiel-Hansen <ezequielhansen.2012@gmail.com>
2025-09-02 16:08:24 -0300

    formulario terminado

--- js/formulario.js
+++ js/formulario.js
@@ -1,46 +1,86 @@
-  const emailElement= document.getElementById("email");
-  const mensajeErrorCodigoPostal= document.getElementById("mensaje_error_codigoPostal");
-  const mensajeErrorElement= document.getElementById("mensaje_error");
-  const paisesElement= document.getElementById("country");
-  const validacionEmail= /^[\w-]+@[\w-]+\.[a-z]{2,4}$/i;
-  const validacionCodigoPostal= /^\d+$/;
-  const codigoPostalElement= document.getElementById("postal-code");
+const emailElement = document.getElementById("email");
+const nombreElement= document.getElementById("nombre");
+const apellidoElement= document.getElementById("apellido");
+const direccionElement= document.getElementById("direccion");
+const ciudadElement= document.getElementById("ciudad");
+const provinciaElement= document.getElementById("provincia");
+const mensajeErrorCodigoPostal = document.getElementById("mensaje_error_codigoPostal");
+const mensajeErrorElement = document.getElementById("mensaje_error");
+const paisesElement = document.getElementById("pais");
+const validacionEmail = /^[\w-]+@[\w-]+\.[a-z]{2,4}$/i;
+const validacionCodigoPostal = /^\d+$/;
+const codigoPostalElement = document.getElementById("postal-code");
+const enviarElement = document.getElementById("btn-enviar-form");
+const valorVacio="";
 
-  codigoPostalElement.addEventListener("input",()=>{
-    if(!validacionCodigoPostal.test(codigoPostalElement.value)){
-        mensajeErrorCodigoPostal.innerText="Codigo Postal Invalido";
-        mensajeErrorCodigoPostal.classList.add("text-red-400");
-    }
-    else{
-        mensajeErrorCodigoPostal.classList.remove("text-red-400");
-        mensajeErrorCodigoPostal.innerText="";
-    }
-  })
+nombreElement.addEventListener("input",bloquearEnviar());
+apellidoElement.addEventListener("input",bloquearEnviar())
+direccionElement.addEventListener("input",bloquearEnviar())
+ciudadElement.addEventListener("input",bloquearEnviar())
+provinciaElement.addEventListener("input",bloquearEnviar())
+
+codigoPostalElement.addEventListener("input", () => {
+  if (!validacionCodigoPostal.test(codigoPostalElement.value)) {
+    mensajeErrorCodigoPostal.innerText = "Codigo Postal Invalido";
+    mensajeErrorCodigoPostal.classList.add("text-red-500");
+  }
+  else {
+    mensajeErrorCodigoPostal.classList.remove("text-red-500");
+    mensajeErrorCodigoPostal.innerText = "";
+  }
+  bloquearEnviar();
+})
 
 
 
-  emailElement.addEventListener("input",()=>{
-    if (!validacionEmail.test(emailElement.value)){
-      mensajeErrorElement.innerText="Email Invalido"
-      mensajeErrorElement.classList.add("text-red-400");
-    }
-    else{
-      mensajeErrorElement.classList.remove("text-red-400");
-      mensajeErrorElement.innerText=""
-    }
-  })
-  
+emailElement.addEventListener("input", () => {
+  if (!validacionEmail.test(emailElement.value)) {
+    mensajeErrorElement.innerText = "Email Invalido"
+    mensajeErrorElement.classList.add("text-red-500");
+  }
+  else {
+    mensajeErrorElement.classList.remove("text-red-500");
+    mensajeErrorElement.innerText = ""
+  }
+  bloquearEnviar();
+})
 
-async function cargaPais(){
+
+async function cargaPais() {
   try {
-    const res= await fetch("https://restcountries.com/v3.1/all?fields=name")
-    const result= await res.json();
-    const paisesNombres= result.map((pais) => pais.name.common).sort()
-    paisesNombres.forEach((e)=>{
-      paisesElement.innerHTML+=`<option value="${e}" >${e}</option>`;
+    const res = await fetch("https://restcountries.com/v3.1/all?fields=name")
+    const result = await res.json();
+    const paisesNombres = result.map((pais) => pais.name.common).sort()
+    paisesNombres.forEach((e) => {
+      paisesElement.innerHTML += `<option value="${e}" >${e}</option>`;
     })
   } catch (error) {
     console.error(error)
   }
-    }
+}
 cargaPais();
+
+function bloquearEnviar (){
+
+  if((emailElement.value, codigoPostalElement.value, nombreElement.value, apellidoElement.value, provinciaElement.value, direccionElement.value, ciudadElement.value)===""){
+    enviarElement.classList.remove("hover:bg-[var(--primary-color)]")
+    enviarElement.classList.remove("hover:text-white")
+    enviarElement.disabled=true;
+  }
+  else{
+    enviarElement.classList.add("hover:bg-[var(--primary-color)]")
+    enviarElement.classList.add("hover:text-white")
+    enviarElement.disabled=false;
+  }
+}
+
+enviarElement.addEventListener("click", () => {
+  Swal.fire({
+    title: 'Enviado',
+    text: 'En breve nos contactaremos con usted',
+    icon: 'success',
+    confirmButtonText: 'OK'
+  })
+})
+
+bloquearEnviar();
